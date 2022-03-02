65a99f8c74f976b145c6cb808879bd0d1834e4ea
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-02 10:12:55 -0700

    server and libraries

--- rowz/server/index.js
+++ rowz/server/index.js
@@ -1,13 +1,29 @@
 const express = require('express')
-const path = require('path')
-const session = require('session')
+// const path = require('path')
+const session = require('express-session')
 const massive = require('massive')
+require('dotenv').config({ path: __dirname + '/../.env'})
+const http = require('http')
+const webSocketServer = require('websocket').server;
 
 // endpoints
 
 
 
 // server
+const { SESSION_SECRET, CONNECTION_STRING, SERVER_PORT } = process.env
+
+const app = express();
+
+app.use(
+    session({
+            resave: true,
+            saveUninitialized: true,
+            secret: SESSION_SECRET,
+            cookie: { maxAge: 1000 * 60 * 5 },
+        }),
+    )
+
 app.use( express.static( __dirname + '/../build'));
 app.get('*', (req,res) => {
 res.send(path.join(__dirname, '../build/index.html'))
@@ -37,7 +53,7 @@ wsServer.on('request', function(request) {
     clients[userID] = connection;
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
-            // console.log('Recieved Message: ',message.utf8Data);
+            console.log('Recieved Message: ',message.utf8Data);
         }
 
         for(key in clients) {
