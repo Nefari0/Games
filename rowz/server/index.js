28ca9244535d16cee9642c3a01cb668b0b88010b
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-02 10:50:24 -0700

    server listens on main port. client listens on socket

--- rowz/server/index.js
+++ rowz/server/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const path = require('path')
+const path = require('path')
 const session = require('express-session')
 const massive = require('massive')
 require('dotenv').config({ path: __dirname + '/../.env'})
