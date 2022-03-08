cc7fb4094ce7cdcc7d736a8c908d3ea818356a36
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-08 13:35:46 -0700

    data

--- rowz/src/Components/pieces.js
+++ rowz/src/Components/pieces.js
@@ -3,6 +3,27 @@ const pieces = [
         id:1,
         player:'good',
         // location:[1][1],
+        x:1,
+        y:0,
+        isInGame:true,
+        isKing:false,
+    },
+
+    {
+        id:2,
+        player:'good',
+        // location:[1][1],
+        x:3,
+        y:0,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:3,
+        player:'good',
+        // location:[1][1],
+        x:2,
+        y:1,
         isInGame:true,
         isKing:false,
     }
