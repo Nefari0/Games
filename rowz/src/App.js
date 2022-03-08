a98abb6d6505f58b761a9f882d47d8cee1d7c41a
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-07 23:44:13 -0700

    display checker board

--- rowz/src/App.js
+++ rowz/src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Home';
+import CheckerBoard from './Components/checkers/CheckerBoard';
 
 function App() {
   return (
     <div className="App">
       {/* <header className="App-header"></header> */}
-      <Home />
+      {/* <Home /> */}
+      <CheckerBoard />
     </div>
   );
 }
