61c1ee370cfb4caa84f830557d94366290abfcb7
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-08 13:37:43 -0700

    renders move options

--- rowz/src/Components/checkers/Tile.js
+++ rowz/src/Components/checkers/Tile.js
@@ -2,34 +2,30 @@ import './tile.css'
 import './CheckerBoard.css'
 const Tile = (props) => {
 
-    const { x,y } = props
-    // console.log(typeof(x),'xcoords')
-    // var elCopy = [...el]
+    const { x,y,color,currentPiece,activeLocation } = props
+    
+    const getCurrent = () => {
+        if(currentPiece[0] != undefined){return currentPiece[0].id}
+    }
 
-    var count = 0
+    // console.log(activeLocation[0] === x && activeLocation[1] === y,'in tile')
 
-    // elCopy.forEach(element => {
-    //     element = count
-    //     count += 1
-    //     console.log(element)
-    // })
-
-    // const mappedEl = elCopy.map(element => {
-    //     element = count
-    //     count += 1
-    //     return <div className="tile-color" key={count} ></div>
-    // })
-
-    // const mappedEl = el.map(element => {
-    //     console.log('are you reading elements',element)
-    //     return <p>{element}</p>
-    // })
-
-    return(<div className='tile-color'>
-    {/* // return(<div className='scrollItems-2'> */}
-        {/* <p>{el.title}</p> */}
+    return(<div className='tile-location'><div className={`tile-color ${color -1 ? true : 'tile-color-dark'} `} onClick={() => props.selectTile(x,y,currentPiece)} >
+    {/* // return(<div> */}
+        {/* <p style={{color:'white'}} >{getCurrent()}</p> */}
+        {currentPiece[0] != undefined ? <div ><p className='player-text'>{currentPiece[0].id}</p></div> : null}
         {/* {mappedEl} */}
         {/* <input type='text' onChange={(e) => {props.changeTitle('title',e.target.value)}} /> */}
+
+    </div>
+        {activeLocation[0] === x && activeLocation[1] === y ? <div className='select-box-overlay'>
+            <div className='low-right' onClick={() => props.setMoves(x+1,y+1,getCurrent())} ></div>
+            <div className='low-left' onClick={() => props.setMoves(x-1,y+1,getCurrent())}></div>
+            <div className='top-left' onClick={() => props.setMoves(x-1,y-1,getCurrent())}></div>
+            <div className='top-right' onClick={() => props.setMoves(x+1,y-1,getCurrent())}></div>
+        </div> : null}
+        {/* {activeLocation[0] === x && activeLocation[1] === y ? <div className='select-box' >
+        </div> : null} */}
     </div>)
 }
 
