93bafc415c7b851c3a06ea44fe36391853915854
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-08 22:28:27 -0700

    hard coded pieces

--- rowz/src/Components/pieces.js
+++ rowz/src/Components/pieces.js
@@ -1,8 +1,9 @@
+
+
 const pieces = [
     {
         id:1,
         player:'good',
-        // location:[1][1],
         x:1,
         y:0,
         isInGame:true,
@@ -12,7 +13,6 @@ const pieces = [
     {
         id:2,
         player:'good',
-        // location:[1][1],
         x:3,
         y:0,
         isInGame:true,
@@ -21,12 +21,189 @@ const pieces = [
     {
         id:3,
         player:'good',
-        // location:[1][1],
+        x:5,
+        y:0,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:4,
+        player:'good',
+        x:7,
+        y:0,
+        isInGame:true,
+        isKing:false,
+    },
+    // ---2
+    {
+        id:5,
+        player:'good',
+        x:6,
+        y:1,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:6,
+        player:'good',
+        x:4,
+        y:1,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:7,
+        player:'good',
         x:2,
         y:1,
         isInGame:true,
         isKing:false,
-    }
+    },
+    {
+        id:8,
+        player:'good',
+        x:0,
+        y:1,
+        isInGame:true,
+        isKing:false,
+    },
+    // ---- 3
+    {
+        id:9,
+        player:'good',
+        x:1,
+        y:2,
+        isInGame:true,
+        isKing:false,
+    },
+
+    {
+        id:10,
+        player:'good',
+        x:3,
+        y:2,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:11,
+        player:'good',
+        x:5,
+        y:2,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:12,
+        player:'good',
+        x:7,
+        y:2,
+        isInGame:true,
+        isKing:false,
+    },
+
+    // bad pieces
+
+    {
+        id:13,
+        player:'bad',
+        x:0,
+        y:7,
+        isInGame:true,
+        isKing:false,
+    },
+
+    {
+        id:14,
+        player:'bad',
+        x:2,
+        y:7,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:15,
+        player:'bad',
+        x:4,
+        y:7,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:16,
+        player:'bad',
+        x:6,
+        y:7,
+        isInGame:true,
+        isKing:false,
+    },
+    // ---2
+    {
+        id:17,
+        player:'bad',
+        x:7,
+        y:6,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:18,
+        player:'bad',
+        x:5,
+        y:6,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:19,
+        player:'bad',
+        x:3,
+        y:6,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:20,
+        player:'bad',
+        x:1,
+        y:6,
+        isInGame:true,
+        isKing:false,
+    },
+    // ---- 3
+    {
+        id:21,
+        player:'bad',
+        x:0,
+        y:5,
+        isInGame:true,
+        isKing:false,
+    },
+
+    {
+        id:22,
+        player:'bad',
+        x:2,
+        y:5,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:23,
+        player:'bad',
+        x:4,
+        y:5,
+        isInGame:true,
+        isKing:false,
+    },
+    {
+        id:24,
+        player:'bad',
+        x:6,
+        y:5,
+        isInGame:true,
+        isKing:false,
+    },
 ]
 
 export default pieces
