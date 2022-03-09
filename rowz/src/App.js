4dd18c13b28e3c76b4affb709520ab15ef24718f
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-09 15:43:37 -0700

    can change between checkers and tic-tac-toe

--- rowz/src/App.js
+++ rowz/src/App.js
@@ -1,14 +1,23 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Home from './Components/Home/Home';
 import CheckerBoard from './Components/checkers/CheckerBoard';
 
 function App() {
+
+  const [playCheckers, setPlayCheckers] = useState(true)
+
+  const tic = 'tic-tac-toe'
+  const check = 'checkers'
+
   return (
     <div className="App">
       {/* <header className="App-header"></header> */}
       {/* <Home /> */}
-      <CheckerBoard />
+      {playCheckers === true ? <CheckerBoard /> : <Home />}
+      {playCheckers === false ? <div className='choose-button' onClick={() => setPlayCheckers(!playCheckers)} ><h4 className='App-h4' >play {check}</h4></div> :  <div className='choose-button-checkers' onClick={() => setPlayCheckers(!playCheckers)} ><h4 className='App-h4' >play {tic}</h4></div>}
+      {/* <div className={`choose-button ${playCheckers ? true : 'choose-botton-checkers'}`}><h4 className='App-h4' >play {check}</h4></div> */}
     </div>
   );
 }
