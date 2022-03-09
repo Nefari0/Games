232faade673121909c40ef6039dc6cbe6e513c38
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-09 15:17:55 -0700

    can cancels "selected tile" function

--- rowz/src/Components/checkers/Tile.js
+++ rowz/src/Components/checkers/Tile.js
@@ -2,7 +2,9 @@ import './tile.css'
 import './CheckerBoard.css'
 const Tile = (props) => {
 
-    const { x,y,color,currentPiece,activeLocation } = props
+    const { x,y,color,currentPiece,activeLocation,currentPlayer,tileIsSelected } = props
+    // const { isKing,player } = activeLocation[2]
+    // console.log('is king',activeLocation[2])
     
     const getCurrent = () => {
         if(currentPiece[0] != undefined){return currentPiece[0].id}
@@ -10,16 +12,17 @@ const Tile = (props) => {
 
     // console.log(activeLocation[0] === x && activeLocation[1] === y,'in tile')
 
-    return(<div className='tile-location'><div className={`tile-color ${color -1 ? true : 'tile-color-dark'} `} onClick={() => props.selectTile(x,y,currentPiece)} >
+    return(<div className='tile-location'  ><div style={{opacity:`${tileIsSelected}`}} className={`tile-color ${activeLocation[0] ? null : 'opac' } ${color -1 ? true : 'tile-color-dark'} `} onClick={() => props.selectTile(x,y,currentPiece)} >
     {/* // return(<div> */}
         {/* <p style={{color:'white'}} >{getCurrent()}</p> */}
-        {currentPiece[0] != undefined ? <div ><p className='player-text'>{currentPiece[0].player}</p></div> : null}
+        {currentPiece[0] != undefined ? <div className={`${currentPiece[0].player === "good" ? 'good-player' : 'bad-player'}`} ><p className='player-text'></p></div> : null}
         {/* {mappedEl} */}
         {/* <input type='text' onChange={(e) => {props.changeTitle('title',e.target.value)}} /> */}
 
     </div>
         {activeLocation[0] === x && activeLocation[1] === y ? <div className='select-box-overlay'>
-            <div className='low-right' onClick={() => props.setMoves(x+1,y+1,getCurrent())} ></div>
+            <div className='cancel-select-tile' onClick={() => {props.unselectTile()}} ><h1 style={{opacity:'.5',color:'#fff'}} >X</h1></div>
+            <div className={`low-right`} onClick={() => props.setMoves(x+1,y+1,getCurrent())} ></div>
             <div className='low-left' onClick={() => props.setMoves(x-1,y+1,getCurrent())}></div>
             <div className='top-left' onClick={() => props.setMoves(x-1,y-1,getCurrent())}></div>
             <div className='top-right' onClick={() => props.setMoves(x+1,y-1,getCurrent())}></div>
