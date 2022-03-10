9c6c05934753fbc8f5b9d617ae63077b33912f36
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-09 23:35:46 -0700

    conditionally rendder items

--- rowz/src/Components/checkers/Tile.js
+++ rowz/src/Components/checkers/Tile.js
@@ -1,17 +1,37 @@
 import './tile.css'
 import './CheckerBoard.css'
+import { useState } from 'react'
 const Tile = (props) => {
 
     const { x,y,color,currentPiece,activeLocation,currentPlayer,tileIsSelected } = props
     // const { isKing,player } = activeLocation[2]
-    // console.log('is king',activeLocation[2])
+
+    // const [fullMoves,setFullMoves] = useState(false)
     
-    const getCurrent = () => {
-        if(currentPiece[0] != undefined){return currentPiece[0].id}
+    
+    // const getCurrent = () => {
+    //     if(currentPiece[0] != undefined){return currentPiece[0].id}
+    // }
+
+    const getCurrent = (params) => {
+        if(currentPiece[0] != undefined) {
+            switch (params) {
+                case 'id':
+                    return currentPiece[0].id;
+                case 'isKing':
+                    return currentPiece[0].isKing;
+                case 'player':
+                    
+                    // console.log('is king',currentPiece[0].isKing + currentPiece[0].player)
+                    // return currentPiece[0].isKing.toString() + currentPiece[0].player.toString()
+            }
+   
+        }
     }
 
+    
     // console.log(activeLocation[0] === x && activeLocation[1] === y,'in tile')
-
+    
     return(<div className='tile-location'  ><div style={{opacity:`${tileIsSelected}`}} className={`tile-color ${activeLocation[0] ? null : 'opac' } ${color -1 ? true : 'tile-color-dark'} `} onClick={() => props.selectTile(x,y,currentPiece)} >
     {/* // return(<div> */}
         {/* <p style={{color:'white'}} >{getCurrent()}</p> */}
@@ -22,10 +42,10 @@ const Tile = (props) => {
     </div>
         {activeLocation[0] === x && activeLocation[1] === y ? <div className='select-box-overlay'>
             <div className='cancel-select-tile' onClick={() => {props.unselectTile()}} ><h1 style={{opacity:'.5',color:'#fff'}} >X</h1></div>
-            <div className={`low-right`} onClick={() => props.setMoves(x+1,y+1,getCurrent())} ></div>
-            <div className='low-left' onClick={() => props.setMoves(x-1,y+1,getCurrent())}></div>
-            <div className='top-left' onClick={() => props.setMoves(x-1,y-1,getCurrent())}></div>
-            <div className='top-right' onClick={() => props.setMoves(x+1,y-1,getCurrent())}></div>
+<div className={`low-right`} onClick={() => props.setMoves(x+1,y+1,getCurrent('id'))} ></div>
+            <div className='low-left' onClick={() => props.setMoves(x-1,y+1,getCurrent('id'))}></div>
+            <div className='top-left' onClick={() => props.setMoves(x-1,y-1,getCurrent('id'))}></div>
+            <div className='top-right' onClick={() => props.setMoves(x+1,y-1,getCurrent('id'))}></div>
         </div> : null}
         {/* {activeLocation[0] === x && activeLocation[1] === y ? <div className='select-box' >
         </div> : null} */}
