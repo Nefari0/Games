70e40a94a30703d51294750705716d688a2bc56b
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-16 09:54:57 -0600

    indicates current turn

---
+++ rowz/src/Components/checkers/CurrentPlayer.js
@@ -0,0 +1,27 @@
+import './CheckerBoard.css'
+import './tile.css'
+import { useEffect,useState } from 'react'
+
+const CurrentPlayer = (props) => {
+
+    const { currentPlayer } = props
+
+    // const [player,setPlayer] = useState(true)
+
+    // useEffect(() => {
+    //     changePiece(currentPlayer)
+    // },[])
+
+    // var changePiece = () => {
+    //     if(currentPlayer === 'good'){
+    //         setPlayer(true)
+    //     } else if (currentPlayer === 'bad') {setPlayer(false)}
+    // }
+
+    return(<div className={`turn-indicator`}>
+        {/* <div className={`good-player ${player ? true : 'bad-player'}`}></div> */}
+        {currentPlayer === 'good' ? <div className='good-player'></div> : <div className='bad-player'></div>}<p className='turn-indicator-text' >current player</p>
+    </div>)
+}
+
+export default CurrentPlayer
