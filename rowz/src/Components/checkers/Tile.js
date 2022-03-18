aa0a277e34da6b91dc3e13b0f3f64da5d2dbd789
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-17 23:07:38 -0600

    working toward animations for enhanced game play

--- rowz/src/Components/checkers/Tile.js
+++ rowz/src/Components/checkers/Tile.js
@@ -33,20 +33,25 @@ const Tile = (props) => {
     // console.log(activeLocation[0] === x && activeLocation[1] === y,'in tile')
     
     return(<div className='tile-location'  ><div style={{opacity:`${tileIsSelected}`}} className={`tile-color ${activeLocation[0] ? null : 'opac' } ${color -1 ? true : 'tile-color-dark'} `} onClick={() => props.selectTile(x,y,currentPiece)} >
-    {/* // return(<div> */}
-        {/* <p style={{color:'white'}} >{getCurrent()}</p> */}
+
         {/* {currentPiece[0] != undefined ? <div className={`${currentPiece[0].player === "good" ? 'good-player' : 'bad-player'}`} > {currentPiece[0].isKing === true ? <p className='player-text'>king</p> : null} </div> : null} */}
-        {currentPiece[0] != undefined ? <div className={`${currentPiece[0].player === "good" ? 'good-player' : 'bad-player'}`} > {currentPiece[0].isKing === true ? <p className='player-text'>king</p> : null} </div> : null}
-        {/* {mappedEl} */}
-        {/* <input type='text' onChange={(e) => {props.changeTitle('title',e.target.value)}} /> */}
+
+        {/* -- animated deaths -- original code above */}
+
+        {currentPiece[0] != undefined ? <div className={`${currentPiece[0].player === "bad"  ? 'bad-player' : 'dead-piece'}`} > {currentPiece[0].isKing === true ? <p className='player-text'>king</p> : null} </div> : null}
+
+        {currentPiece[0] != undefined ? <div className={`${currentPiece[0].player === "good" ? 'good-player' : ''}`} > {currentPiece[0].isKing === true ? <p className='player-text'>king</p> : null} </div> : null}
+        
+        
+        
 
     </div>
         {activeLocation[0] === x && activeLocation[1] === y ? <div className='select-box-overlay'>
             <div className='cancel-select-tile' onClick={() => {props.unselectTile()}} ><h1 style={{opacity:'.5',color:'#fff'}} >X</h1></div>
-            <div className={`low-right`} onClick={() => props.setMoves(x+1,y+1,getCurrent('id'),activeLocation,true,currentPlayer,pieces,getCurrent('isKing'))} ></div>
-            <div className='low-left' onClick={() => props.setMoves(x-1,y+1,getCurrent('id'),activeLocation,true,currentPlayer,pieces,getCurrent('isKing'))}></div>
-            <div className='top-left' onClick={() => props.setMoves(x-1,y-1,getCurrent('id'),activeLocation,true,currentPlayer,pieces,getCurrent('isKing'))}></div>
-            <div className='top-right' onClick={() => props.setMoves(x+1,y-1,getCurrent('id'),activeLocation,true,currentPlayer,pieces,getCurrent('isKing'))}></div>
+            <div className={`low-right`} onClick={() => props.setMoves(x+1,y+1,getCurrent('id'),activeLocation,true,currentPlayer,pieces,getCurrent('isKing'),currentPiece)} ></div>
+            <div className='low-left' onClick={() => props.setMoves(x-1,y+1,getCurrent('id'),activeLocation,true,currentPlayer,pieces,getCurrent('isKing'),currentPiece)}></div>
+            <div className='top-left' onClick={() => props.setMoves(x-1,y-1,getCurrent('id'),activeLocation,true,currentPlayer,pieces,getCurrent('isKing'),currentPiece)}></div>
+            <div className='top-right' onClick={() => props.setMoves(x+1,y-1,getCurrent('id'),activeLocation,true,currentPlayer,pieces,getCurrent('isKing'),currentPiece)}></div>
         </div> : null}
         {/* {activeLocation[0] === x && activeLocation[1] === y ? <div className='select-box' >
         </div> : null} */}
