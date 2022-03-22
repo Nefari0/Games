c8a72805e81ccc6d12c620bd6e883cb2b6d73ef3
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-22 10:34:48 -0600

    updates

--- rowz/src/Components/Notice/Notice.js
+++ rowz/src/Components/Notice/Notice.js
@@ -8,6 +8,8 @@ import { useState,useEffect } from 'react'
 
 const Notice = (props) => {
 
+    const { x,y, activeLocation, currentPiece, pieces } = props
+
     // useEffect(() => {
     //     setShowNotice(false)
     // },[])
@@ -30,6 +32,14 @@ const Notice = (props) => {
     return (
         <div className={`notice-container ${item} ${showNotice ? true : 'hidden'}`} onClick={() => setShowNotice()} >
             <p style={{color:'#fff',textTransform:'none'}}> here it is{content}</p>
+
+        {activeLocation[0] === x && activeLocation[1] === y ? <div className='select-box-overlay'>
+        <div className='cancel-select-tile' onClick={() => {props.unselectTile()}} ><h1 style={{opacity:'.5',color:'#fff'}} >X</h1></div>
+        <div className={`low-right`} onClick={() => props.setMoves(x+1,y+1,props.getCurrent('id'),activeLocation,true,currentPlayer,pieces,props.getCurrent('isKing'),currentPiece)} ></div>
+        <div className='low-left' onClick={() => props.setMoves(x-1,y+1,props.getCurrent('id'),activeLocation,true,currentPlayer,pieces,props.getCurrent('isKing'),currentPiece)}></div>
+        <div className='top-left' onClick={() => props.setMoves(x-1,y-1,props.getCurrent('id'),activeLocation,true,currentPlayer,pieces,props.getCurrent('isKing'),currentPiece)}></div>
+        <div className='top-right' onClick={() => props.setMoves(x+1,y-1,props.getCurrent('id'),activeLocation,true,currentPlayer,pieces,props.getCurrent('isKing'),currentPiece)}></div>
+    </div> : null}
         </div>
     )
 }
