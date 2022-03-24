ba0074ac49790315f2703b413e8f089738605c13
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-24 09:05:14 -0600

    new "piece" rendering methods

--- rowz/src/Components/checkers/Tile.js
+++ rowz/src/Components/checkers/Tile.js
@@ -1,24 +1,26 @@
 import './tile.css'
 import './CheckerBoard.css'
-import { useState } from 'react'
-import Notice from '../Notice/Notice'
+import { useState,useEffect, useRef } from 'react'
+// import Notice from '../Notice/Notice'
+import Piece from './Piece'
 const Tile = (props) => {
 
-    // const [inPlay,setInPlay] = useState(false)
-
+    
     const { x,y,color,currentPiece,activeLocation,currentPlayer,tileIsSelected,pieces,chainKillAvailable,moveOptions } = props
+
+    // const [inPlay,setInPlay] = useState(false)
+    // const [makeMove,setMakeMove] = useState(false)
+
+    const prevPiece = useRef()
+
+    // useEffect(() => {
+    //     prevPiece.alive = currentPiece[0]
+    // })
     
     if(moveOptions.length > 0){
         if ([x+1,y+1] === moveOptions) {console.log('here is the move',x+1, moveOptions)}
     }
-
-    // const [makeMove,setMakeMove] = useState(false)
     
-    
-    // const getCurrent = () => {
-    //     if(currentPiece[0] != undefined){return currentPiece[0].id}
-    // }
-
     const getCurrent = (params) => {
         if(currentPiece[0] !== undefined) {
             switch (params) {
@@ -28,37 +30,33 @@ const Tile = (props) => {
                     return currentPiece[0].isKing;
                 case 'pendingDeath':
                     return currentPiece[0].pendingDeath
+                case 'isInGame':
+                    return currentPiece[0].isInGame
+                case 'player':
+                    return currentPiece[0].player
             }
    
         }
     }
 
-    if(getCurrent('pendingDeath') === true){console.log('pending death',getCurrent('id'))}
-    // console.log(activeLocation[0] === x && activeLocation[1] === y,'in tile')
-    
-    // return(<div className='tile-location'  ><div style={{opacity:`${tileIsSelected}`}} className={`tile-color ${activeLocation[0] ? null : 'opac' } ${color -1 ? true : 'tile-color-dark'} `} onClick={() => props.selectTile(x,y,currentPiece)} >
     return(<div className='tile-location'  ><div className={`tile-color ${!activeLocation[0] ? 'hide-opac' : 'display-opac' } ${color -1 ? true : 'tile-color-dark'} `} onClick={() => props.selectTile(x,y,currentPiece)} >
 
-        {/* {currentPiece[0] != undefined ?<Notice setMoves={props.setMoves} getCurrent={getCurrent} x={x} y={y} activeLocation={activeLocation} pieces={pieces} /> : null} */}
 
-        {/* {currentPiece[0] != undefined ? <div className={`${currentPiece[0].player === "good" ? 'good-player' : 'bad-player'}`} > {currentPiece[0].isKing === true ? <p className='player-text'>king</p> : null} </div> : null} */}
+        {/* {currentPiece[0] !== undefined ? <div className={`${currentPiece[0].player === "bad" ? 'bad-player' : 'dead-piece'}`} > {currentPiece[0].isKing === true ? <p className='player-text'>king</p> : null} </div> : null} */}
 
-        {/* -- animated deaths -- original code above */}
+        {/* {currentPiece[0] !== undefined ? <div className={`${currentPiece[0].player === "good" ? 'good-player' : 'dead-piece'}`} > {currentPiece[0].isKing === true ? <p className='player-text'>king</p> : null} </div> : null} */}
 
-        {currentPiece[0] !== undefined ? <div className={`${currentPiece[0].player === "bad" ? 'bad-player' : 'dead-piece'}`} > {currentPiece[0].isKing === true ? <p className='player-text'>king</p> : null} </div> : null}
-        {/* {currentPiece[0] != undefined ? <div className={`${currentPiece[0].player === "bad"  ? 'bad-player' : 'dead-piece'}`} > {currentPiece[0].isKing === true ? <p className='player-text'>king</p> : null} </div> : <div className={`notice-container ${inPlay ? true : 'hidden'}`}></div>} */}
-
-        {currentPiece[0] !== undefined ? <div className={`${currentPiece[0].player === "good" ? 'good-player' : 'dead-piece'}`} > {currentPiece[0].isKing === true ? <p className='player-text'>king</p> : null} </div> : null}
+        {/* ------- animated deaths ------- original code above ----*/}
+        <Piece setMoves={props.setMoves} prevPiece={prevPiece.current} getCurrent={getCurrent} x={x} y={y} activeLocation={activeLocation} pieces={pieces} currentPiece={currentPiece}/>
 
         {getCurrent('pendingDeath') === true && currentPiece[0].player === "bad" ? 
-        // <p className='player-text dead'>:-(</p>
+        
         <svg xmlns="http://www.w3.org/2000/svg" className="emo-head-bad" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
          : null}
 
         {getCurrent('pendingDeath') === true && currentPiece[0].player === "good" ? 
-        // <p className='player-text dead'>:-(</p>
         <svg xmlns="http://www.w3.org/2000/svg" className="emo-head-good" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
