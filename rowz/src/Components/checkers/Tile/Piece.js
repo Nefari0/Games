da23822568b79a4b2c313512d101e92cd91e8d12
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-06 07:49:46 -0600

    Preparing for major code changes

--- rowz/src/Components/checkers/Tile/Piece.js
+++ rowz/src/Components/checkers/Tile/Piece.js
@@ -6,7 +6,7 @@ const Piece = (props) => {
     return (
         <div>
             {props.getCurrent('player') ==='good' ? 
-            <div className={`good-player ${props.getCurrent('isInGame') ? false : 'hidden'}`}>{props.getCurrent('isKing') === true ? <p className='player-text'>king</p> : null}</div>
+            <div className={`good-player ${props.getCurrent('isInGame') ? false : `hidden`}`}>{props.getCurrent('isKing') === true ? <p className='player-text'>king</p> : null}</div>
         :
             <div className={`bad-player ${props.getCurrent('isInGame') ? false : 'hidden'}`}>{props.getCurrent('isKing') === true ? <p className='player-text'>king</p> : null}</div>}
             
