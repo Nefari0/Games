ecc632e6b13e91e08740b49cd1ac5af22c8146b6
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-07 14:15:53 -0600

    Piece locations displayed from this component.

---
+++ rowz/src/Components/checkers/Tile/Piece/piece.component.jsx
@@ -0,0 +1,36 @@
+import { SadFace, SmileFace } from './SVG'
+import { GoodPlayer,BadPlayer } from './piece.styles'
+import Crown from '../Crown/Crown'
+import '../tile.css'
+
+const Piece = ({items}) => {    
+
+    const {isKing,player,x,y,pendingDeath} = items
+
+    const engine = {
+        left:`${7+(x*42)}px`,
+        top:`${3+(y*42)}px`,
+        position:'absolute',
+        transition: "all 1000ms",
+    }
+
+    return (
+        <div style={engine}>
+
+            {player ==='good' ? 
+            <GoodPlayer>
+                {isKing && <Crown />}
+                {pendingDeath && SadFace('#fff')}
+            </GoodPlayer>
+            :
+            <BadPlayer>
+                {pendingDeath && SadFace('#555')}
+                {isKing && <Crown />}
+            </BadPlayer>
+            }
+            
+        </div>
+    )
+}
+
+export default Piece
