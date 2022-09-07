025f383b18079c2d7a1cb07b9782299154a6e05a
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-07 14:18:50 -0600

    Updating and cleaning

--- rowz/src/App.js
+++ rowz/src/App.js
@@ -1,10 +1,7 @@
-// import logo from './logo.svg';
-// const sass = require('sass');
 import { useState } from 'react';
 import './App.css';
 import Home from './Components/Home/Home';
-import CheckerBoard from './Components/checkers/CheckerBoard';
-import Crown from './Components/checkers/Tile/Crown/Crown';
+import CheckerBoard from './Components/checkers/CheckerBoard/CheckerBoard';
 
 function App() {
 
@@ -15,13 +12,20 @@ function App() {
 
   return (
     <div className="App">
-      {/* <header className="App-header"></header> */}
-      {/* <Home /> */}
-    {/* <Crown /> */}
-      {playCheckers === true ? <CheckerBoard /> : <Home />}
-      {playCheckers === false ? <div className='choose-button' onClick={() => setPlayCheckers(!playCheckers)} ><h4 className='App-h4' >play {check}</h4></div> :  <div className='choose-button-checkers' onClick={() => setPlayCheckers(!playCheckers)} ><h4 className='App-h4' >play {tic}</h4></div>}
 
-      {/* <div className={`choose-button ${playCheckers ? true : 'choose-botton-checkers'}`}><h4 className='App-h4' >play {check}</h4></div> */}
+      {playCheckers === true ? <CheckerBoard /> : <Home />}
+
+      {
+      playCheckers === false ?
+      <div className='choose-button' onClick={() => setPlayCheckers(!playCheckers)} >
+        <h4 className='App-h4' >play {check}</h4>
+      </div>
+      :
+      <div className='choose-button-checkers' onClick={() => setPlayCheckers(!playCheckers)} >
+        <h4 className='App-h4' >play {tic}</h4>
+      </div>
+      }
+
     </div>
   );
 }
