f38e8ad435664199e92d515a2de4c4cc0cfd65b5
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-13 23:07:05 -0600

    Add move constraints for chained attacks

--- rowz/src/Components/checkers/Tile/ControlPanel/controls.component.jsx
+++ rowz/src/Components/checkers/Tile/ControlPanel/controls.component.jsx
@@ -1,4 +1,5 @@
 import { CloseButton } from "../Piece/SVG"
+import { useState } from "react"
 import {
     ControlBox,
     LowRight,
@@ -18,8 +19,32 @@ export const Controller = (props) => {
         currentPlayer,
         currentPiece,
         pieces,
-        x,y
+        x,y,
+        chainKillAvailable,
+        moveOptions,
+        previousPiece,
     } = props
+
+    const selectionConstraint = (coords) => {
+        if (activeLocation[2] != undefined && previousPiece != null) {
+            console.log('NOT UNDEFINED!',coords[1],y-1)
+            if (currentPiece[0].id === previousPiece.id && moveOptions[0] != undefined) {
+                if (coords[0] === x+1 && coords[1] === y-1) {
+                    console.log('true',)
+                    return 'top_right'
+                } else if (coords[0] === x+1 && coords[1] === y+1) {
+                    return 'low_right'
+                } else if (coords[0] === x-1 && coords[1] === y+1) {
+                    return 'low_left'
+                } else if (coords[0] === x-1 && coords[1] === y-1) {
+                    return 'top_left'
+                }
+                // x-1,y-1
+            }
+        }
+    }
+
+    // selectionConstraint()
 
     const actuator = (x,y) => {
         const id = getCurrent('id')
@@ -28,15 +53,29 @@ export const Controller = (props) => {
     }
 
     return (
+        <>
+       
         <ControlBox>
+            
             <CloseController onClick={() => unselectTile()}>
                 {CloseButton('#fff')}
             </CloseController>
-            <LowRight onClick={() => actuator(x+1,y+1)} />
-            <LowLeft onClick={() => actuator(x-1,y+1)} />
-            <TopLeft onClick={() => actuator(x-1,y-1)} />
-            <TopRight onClick={() => actuator(x+1,y-1)} />
+
+            {!chainKillAvailable || selectionConstraint(moveOptions) === 'low_right' ?
+            <LowRight onClick={() => actuator(x+1,y+1)} />:null}
+
+            {!chainKillAvailable || selectionConstraint(moveOptions) === 'low_left' ?
+            <LowLeft onClick={() => actuator(x-1,y+1)} />:null}
+
+            {!chainKillAvailable || selectionConstraint(moveOptions) === 'top_left' ?
+            <TopLeft onClick={() => actuator(x-1,y-1)} />:null}
+
+            {!chainKillAvailable || selectionConstraint(moveOptions) === 'top_right' ?
+            <TopRight onClick={() => actuator(x+1,y-1)} />:null}
+
         </ControlBox>
+ 
+        </>
     )
 }
 
