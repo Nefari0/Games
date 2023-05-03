266181acd4865cb225935d8b4fa78233b4cacb24
Nefari0 <chrisdemontigny@yahoo.com>
2023-05-02 21:10:18 -0600

    re-configuration

--- rowz/src/redux/store.js
+++ rowz/src/redux/store.js
@@ -1,15 +1,23 @@
-import { createStore,combineReducers,applyMiddleware } from "redux";
+import { createStore,compose,combineReducers,applyMiddleware } from "redux";
+import logger from "redux-logger";
 import reduxPromiseMiddleware from 'redux-promise-middleware'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import checkerReducer from "./checkerReducer";
 import globalReducer from "./globalReducer";
+
+const middlewares = [logger]
 
 const rootReducer = combineReducers({
     currentPlayer:checkerReducer,
     globalReducer:globalReducer,
 })
 
+const composedEnhancers = compose(applyMiddleware(...middlewares))
+
 export default createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(reduxPromiseMiddleware))
+    undefined,
+    composedEnhancers,
+    // middlewares,
+    // composeWithDevTools(applyMiddleware(reduxPromiseMiddleware))
 );
